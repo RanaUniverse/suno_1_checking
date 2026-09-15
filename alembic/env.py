3beecabb15159c73f19cccd51f167b2475e3f161
RanaUniverse <RanaUniverse321@gmail.com>
2026-09-16 00:16:08 +0530

    adding the database url in the env.py

--- alembic/env.py
+++ alembic/env.py
@@ -3,11 +3,28 @@ from logging.config import fileConfig
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
+from sqlmodel import SQLModel
+
+from app.config import settings
+
+from app.features.identity.infrastructure.sqlmodel.models import UserModel
+
+
 from alembic import context
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
+
+DATABASE_URL = settings.db.db_url
+
+
 config = context.config
+
+config.set_main_option(
+    name="sqlalchemy.url",
+    value=DATABASE_URL,
+)
+
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -18,7 +35,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = SQLModel.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -64,9 +81,7 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
